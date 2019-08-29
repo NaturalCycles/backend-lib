8971ccfb906c05518f41fdba3ab728c2c526c6c1
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-08-29 19:05:21 +0200

    feat: revert serviceUrl to include 'default'

--- src/gae/deploy.util.ts
+++ src/gae/deploy.util.ts
@@ -212,15 +212,9 @@ export async function createDeployInfo (backendCfg: BackendCfg): Promise<DeployI
     ].join('-')
   }
 
-  const gaeServiceInUrl = gaeService === 'default' ? undefined : gaeService
+  const versionUrl = `https://${[gaeVersion, gaeService, gaeProject].join('-dot-')}.appspot.com`
 
-  const versionUrl = `https://${[gaeVersion, gaeServiceInUrl, gaeProject]
-    .filter(Boolean)
-    .join('-dot-')}.appspot.com`
-
-  const serviceUrl = `https://${[gaeServiceInUrl, gaeProject]
-    .filter(Boolean)
-    .join('-dot-')}.appspot.com`
+  const serviceUrl = `https://${[gaeService, gaeProject].join('-dot-')}.appspot.com`
 
   const deployInfo: DeployInfo = {
     gaeProject,
