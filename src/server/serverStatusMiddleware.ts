47b2b012ba507f4aedae627005cde85039a60df7
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-12-28 14:32:36 +0100

    feat: statusMiddleware reports NODE_OPTIONS and fetch support

--- src/server/serverStatusMiddleware.ts
+++ src/server/serverStatusMiddleware.ts
@@ -1,10 +1,10 @@
-import { _filterFalsyValues, localTime } from '@naturalcycles/js-lib'
+import { _filterNullishValues, localTime } from '@naturalcycles/js-lib'
 import { memoryUsageFull, processSharedUtil } from '@naturalcycles/nodejs-lib'
 import { getDeployInfo } from './deployInfo.util'
 import { BackendRequestHandler } from './server.model'
 
 const { versions } = process
-const { GAE_APPLICATION, GAE_SERVICE, GAE_VERSION, APP_ENV } = process.env
+const { GAE_APPLICATION, GAE_SERVICE, GAE_VERSION, APP_ENV, NODE_OPTIONS } = process.env
 
 export function serverStatusMiddleware(projectDir?: string, extra?: any): BackendRequestHandler {
   return async (req, res) => {
@@ -21,7 +21,7 @@ export function getServerStatusData(
   const deployBuildTime = t.toPretty()
   const buildInfo = [t.toStringCompact(), gitBranch, gitRev].filter(Boolean).join('_')
 
-  return _filterFalsyValues({
+  return _filterNullishValues({
     started: getStartedStr(),
     deployBuildTime,
     APP_ENV,
@@ -34,6 +34,8 @@ export function getServerStatusData(
     cpuAvg: processSharedUtil.cpuAvg(),
     // resourceUsage: process.resourceUsage?.(),
     versions,
+    NODE_OPTIONS,
+    fetch: typeof globalThis.fetch === 'function',
     ...extra,
   })
 }
