7ecdef89f34884baf9cd4dc720d9a75d0adff5a5
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-12-30 03:46:52 +0100

    feat: zodReqValidate

---
+++ src/server/zodValidateMiddleware.ts
@@ -0,0 +1,57 @@
+import {
+  HttpError,
+  _get,
+  ZodSchema,
+  ZodValidationError,
+  zSafeValidate,
+} from '@naturalcycles/js-lib'
+import { BackendRequestHandler } from './server.model'
+import { ReqValidationOptions } from './reqValidationMiddleware'
+
+const REDACTED = 'REDACTED'
+
+/**
+ * Validates req property (body, params or query).
+ * Supports Joi schema or AjvSchema (from nodejs-lib).
+ * Able to redact sensitive data from the error message.
+ * Throws http 400 on error.
+ */
+export function zodReqValidate(
+  prop: 'body' | 'params' | 'query',
+  schema: ZodSchema,
+  opt: ReqValidationOptions<ZodValidationError<any>> = {},
+): BackendRequestHandler {
+  const reportPredicate = typeof opt.report === 'function' ? opt.report : () => !!opt.report
+
+  return (req, res, next) => {
+    const { error } = zSafeValidate(req[prop], schema)
+    if (!error) {
+      return next()
+    }
+
+    const report = reportPredicate(error)
+
+    if (opt.redactPaths) {
+      redact(opt.redactPaths, req[prop], error)
+    }
+
+    return next(
+      new HttpError(error.message, {
+        httpStatusCode: 400,
+        report,
+      }),
+    )
+  }
+}
+
+/**
+ * Mutates error
+ */
+function redact(redactPaths: string[], obj: any, error: Error): void {
+  redactPaths
+    .map(path => _get(obj, path) as string)
+    .filter(Boolean)
+    .forEach(secret => {
+      error.message = error.message.replace(secret, REDACTED)
+    })
+}
