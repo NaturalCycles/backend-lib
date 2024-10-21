3e07603e07d12b9ce044f6544467f59cc2837370
David Nagy <mrnagydavid@users.noreply.github.com>
2024-10-21 15:00:23 +0200

    feat: add `validateHeaders` middleware

--- src/server/validation/validateMiddleware.test.ts
+++ src/server/validation/validateMiddleware.test.ts
@@ -1,5 +1,8 @@
+import { jsonSchema, StringMap } from '@naturalcycles/js-lib'
 import { debugResource } from '../../test/debug.resource'
-import { expressTestService } from '../../testing'
+import { ExpressApp, expressTestService } from '../../testing'
+import { getDefaultRouter } from '../getDefaultRouter'
+import { validateHeaders } from './validateMiddleware'
 
 const app = expressTestService.createAppFromResource(debugResource)
 afterAll(async () => {
@@ -38,4 +41,103 @@ Input: { pw: 'REDACTED' }",
   "name": "AppError",
 }
 `)
+})
+
+describe('validateHeader', () => {
+  let app: ExpressApp
+  interface TestResponse {
+    ok: 1
+    headers: StringMap<any>
+  }
+
+  beforeAll(async () => {
+    const resource = getDefaultRouter()
+    const schema = jsonSchema.object({
+      shortstring: jsonSchema.string().min(8).max(16),
+      numeric: jsonSchema.string(),
+      bool: jsonSchema.string(),
+      sessionid: jsonSchema.string(),
+    })
+    resource.get('/', validateHeaders(schema, { redactPaths: ['sessionid'] }), async (req, res) => {
+      res.json({ ok: 1, headers: req.headers })
+    })
+    app = expressTestService.createAppFromResource(resource)
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should pass valid headers', async () => {
+    const response = await app.get<TestResponse>('', {
+      headers: {
+        shortstring: 'shortstring',
+        numeric: '123',
+        bool: '1',
+        sessionid: 'sessionid',
+      },
+    })
+
+    expect(response).toMatchObject({ ok: 1 })
+    expect(response.headers).toEqual({
+      shortstring: 'shortstring',
+      numeric: '123',
+      bool: '1',
+      sessionid: 'sessionid',
+    })
+  })
+
+  test('should throw error on invalid headers', async () => {
+    const err = await app.expectError({
+      url: '',
+      method: 'GET',
+      headers: {
+        shortstring: 'short',
+        numeric: '123',
+        bool: '1',
+        sessionid: 'sessionid',
+      },
+    })
+
+    expect(err.data.responseStatusCode).toBe(400)
+    expect(err.cause.message).toContain(
+      'request headers/shortstring must NOT have fewer than 8 characters',
+    )
+  })
+
+  test('should list all errors (and not stop at the first error)', async () => {
+    const err = await app.expectError({
+      url: '',
+      method: 'GET',
+      headers: {
+        shortstring: 'short',
+        // numeric: '123',
+        bool: '1',
+        sessionid: 'sessionid',
+      },
+    })
+
+    expect(err.data.responseStatusCode).toBe(400)
+    expect(err.cause.message).toContain(
+      'request headers/shortstring must NOT have fewer than 8 characters',
+    )
+    expect(err.cause.message).toContain("request headers must have required property 'numeric'")
+  })
+
+  test('should redact sensitive data', async () => {
+    const err = await app.expectError({
+      url: '',
+      method: 'GET',
+      headers: {
+        shortstring: 'short',
+        numeric: '127',
+        bool: '1',
+        sessionid: 'sessionid',
+      },
+    })
+
+    expect(err.data.responseStatusCode).toBe(400)
+    expect(err.cause.message).toContain("REDACTED: 'REDACTED'")
+    expect(err.cause.message).not.toContain('sessionid')
+  })
 })
