f34d7d8bb1bef09b0eb8477489ebe20089af55b5
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-03-23 14:28:18 +0100

    fix: clarify logMiddleware

--- src/server/logMiddleware.ts
+++ src/server/logMiddleware.ts
@@ -3,9 +3,11 @@ import { AnyObject, CommonLogger } from '@naturalcycles/js-lib'
 import { _inspect, dimGrey } from '@naturalcycles/nodejs-lib'
 import { BackendRequestHandler } from './server.model'
 
-const { GOOGLE_CLOUD_PROJECT, GAE_INSTANCE, K_SERVICE } = process.env
+const { GOOGLE_CLOUD_PROJECT, GAE_INSTANCE, APP_ENV } = process.env
 const isGAE = !!GAE_INSTANCE
-const isCloudRun = !!K_SERVICE
+// const isCloudRun = !!K_SERVICE
+// const isTest = APP_ENV === 'test'
+const isDev = APP_ENV === 'dev'
 
 // Simple "request counter" (poor man's "correlation id") counter, to use on dev machine (not in the cloud)
 let reqCounter = 0
@@ -68,17 +70,39 @@ function logToCI(args: any[]): void {
 }
 
 export function logMiddleware(): BackendRequestHandler {
-  if (isCloudRun) {
-    // Cloud Run, return simple logger, similar to CI logger
-    return function cloudRunLogHandler(req, _res, next) {
-      req.log = req.warn = req.error = (...args: any[]) => logToCI(args)
+  if (isGAE) {
+    if (GOOGLE_CLOUD_PROJECT) {
+      return function appEngineLogHandler(req, _res, next) {
+        const traceHeader = req.header('x-cloud-trace-context')
+        if (traceHeader) {
+          const [trace] = traceHeader.split('/')
+          const meta = {
+            'logging.googleapis.com/trace': `projects/${GOOGLE_CLOUD_PROJECT}/traces/${trace}`,
+            'appengine.googleapis.com/request_id': req.header('x-appengine-request-log-id'),
+          }
+          Object.assign(req, {
+            log: (...args: any[]) => logToAppEngine({ ...meta, severity: 'INFO' }, args),
+            warn: (...args: any[]) => logToAppEngine({ ...meta, severity: 'WARNING' }, args),
+            error: (...args: any[]) => logToAppEngine({ ...meta, severity: 'ERROR' }, args),
+          })
+          req.requestId = trace
+        } else {
+          Object.assign(req, gaeLogger)
+        }
+
+        next()
+      }
+    }
+
+    return function appEngineLogHandler(req, _res, next) {
+      Object.assign(req, gaeLogger)
       next()
     }
   }
 
-  if (!isGAE || !GOOGLE_CLOUD_PROJECT) {
+  if (isDev) {
     // Local machine, return "simple" logToDev middleware with request numbering
-    return function gaeLogMiddlewareDev(req, _res, next) {
+    return function devLogHandler(req, _res, next) {
       // Local machine
       req.requestId = String(++reqCounter)
       req.log = req.warn = req.error = (...args: any[]) => logToDev(req.requestId!, args)
@@ -86,26 +110,10 @@ export function logMiddleware(): BackendRequestHandler {
     }
   }
 
-  // Otherwise, we're in AppEngine
-
-  return function appEngineLogHandler(req, _res, next) {
-    const traceHeader = req.header('x-cloud-trace-context')
-    if (traceHeader) {
-      const [trace] = traceHeader.split('/')
-      const meta = {
-        'logging.googleapis.com/trace': `projects/${GOOGLE_CLOUD_PROJECT}/traces/${trace}`,
-        'appengine.googleapis.com/request_id': req.header('x-appengine-request-log-id'),
-      }
-      Object.assign(req, {
-        log: (...args: any[]) => logToAppEngine({ ...meta, severity: 'INFO' }, args),
-        warn: (...args: any[]) => logToAppEngine({ ...meta, severity: 'WARNING' }, args),
-        error: (...args: any[]) => logToAppEngine({ ...meta, severity: 'ERROR' }, args),
-      })
-      req.requestId = trace
-    } else {
-      Object.assign(req, gaeLogger)
-    }
-
+  // Otherwise, return "simple" logger
+  // This includes: unit tests, CloudRun, CI environments
+  return function simpleLogHandler(req, _res, next) {
+    req.log = req.warn = req.error = (...args: any[]) => logToCI(args)
     next()
   }
 }
